3121446e074ef9670250aa35aeba90a416ea7c11
Andrew Pong <a@awp.io>
2017-04-13 11:52:07 -0700

    update build script with dev deps and test execution command

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+# install dev dependencies
+# add colors to nosetests
+pip install rednose
+
 # clean ./build
 python setup.py clean --all
 
@@ -6,3 +10,9 @@ python setup.py sdist
 
 # build python wheel
 python setup.py bdist_wheel
+
+# install bdist_wheel
+pip install dist/osisoft_pi_webapi_python_client-1.1.0-py2.py3-none-any.whl
+
+# run tests
+nosetests --rednose
