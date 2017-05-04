71f32d317fa6e2d6c9b37cff407ec7893752af15
Andrew Pong <a@awp.io>
2017-05-04 15:26:15 -0700

    moved to virtualenv

--- setup.py
+++ setup.py
@@ -1,24 +1,19 @@
-# try:
-#     from setuptools import setup
-# except ImportError:
-#     from distutils.core import setup
+# -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
 
 setup(
     name='osisoftpy',
     description='OSIsoft PI Web WebAPI client',
-    version='1.2.0',
+    version='2.0.0',
     url='https://gitlab.com/dstcontrols/pge-piclient-python',
     license='MIT',
-    author='Andrew Pong <apong@dstcontrols.com>, Alan Kenyon <akenyon@dstcontrols.com>',
+    author='Andrew Pong <apong@dstcontrols.com>',
     install_requires=[
-        'nose',
         'requests',
         'requests-kerberos',
         'rx', 'colorlog', 'six', 'arrow', 'pytest'
     ],
-    # packages=['OSIsoftPy'],
     packages=find_packages(),
     scripts=[],
 )
